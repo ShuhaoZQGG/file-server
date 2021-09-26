c2b613e5587da35ea7e62be8edb615544ad9921a
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-26 00:35:37 -0400

    requestFile from client and modular files

--- client.js
+++ client.js
@@ -1,16 +1,29 @@
 const net = require('net');
+const {host, port, name} = require('./constant');
 
-const conn = net.createConnection({ 
-  host: 'localhost', // change to IP address of computer or ngrok host if tunneling
-  port: 3000 // or change to the ngrok port if tunneling
-});
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: host, // change to IP address of computer or ngrok host if tunneling
+    port: port // or change to the ngrok port if tunneling
+  });
 
-conn.setEncoding('utf8'); // interpret data as text
+  conn.setEncoding('utf8'); // interpret data as text
 
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
-});
+  const myName = name;
 
-conn.on('connect', () => {
-  conn.write('Hello from client!');
-});
+  conn.on('connect', () => {
+    conn.write(`Hello from client ${myName}!`);
+  });
+
+
+  conn.on('data', (data) => {
+    console.log("DATA CAME IN!!!!!");
+    console.log(data);
+  })
+
+  return conn;
+}
+
+module.exports = {
+  connect
+}
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const net = require('net');
 
-const server = net.createServer();
 
-server.on('connection', (client) => {
-  console.log('New client connected!');
-  client.write('Hello there!');
+const setServer = function() {
+  const server = net.createServer();
+  server.on('connection', (client) => {
+    console.log('New client connected!');
+    client.write('Hello there! Please enter the file name that you want to retrieve:');
 
-  client.setEncoding('utf8'); // interpret data as text
-  client.on('data', (data) => {
-    console.log('Message from client: ', data)
+    client.setEncoding('utf8'); // interpret data as text
+    client.on('data', (data) => {
+      console.log('Message from client: ', data)
+    });
   });
-});
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000!');
-});
+  server.listen(3000, () => {
+    console.log('Server listening on port 3000!');
+  });
+
+  return server;
+}
+
+module.export = {
+  setServer
+}
